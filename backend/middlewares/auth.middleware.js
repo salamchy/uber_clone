33b6e7e31641f5abb32c8c15bb088b70bbfa9207
salam <chaudharysm00@gmail.com>
2024-12-25 22:41:07 +0545

    captain authentication

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
 import { userModel } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { BlacklistTokenModel } from "../models/blacklistToken.model.js";
+import { captainModel } from "../models/captain.model.js";
 
 export const authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -8,9 +10,9 @@ export const authUser = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized to access this route" });
   }
 
-  const isBlacklisted = await userModel.findOne({ token: token});
+  const isBlacklisted = await BlacklistTokenModel.findOne({ token: token});
 
-  if(isBlacklisted) {
+  if (isBlacklisted) {
     return res.status(401).json({ message: "Unauthorized to access this route" });
   }
 
@@ -27,3 +29,31 @@ export const authUser = async (req, res, next) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized to access this route" });
+  }
+
+  const isBlacklisted = await BlacklistTokenModel.findOne({token: token});
+
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized " });
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    const captain = await captainModel.findById(decode._id);
+
+    req.captain = captain;
+    return next();
+
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+}
